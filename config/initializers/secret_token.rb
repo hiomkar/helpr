c8ca6289d5ba7cd0aab97badcd43d50e65b9ac5e
Omkar <hiomkar@gmail.com>
2012-10-30 16:07:59 -0400

    creating helpr rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpr::Application.config.secret_token = 'aefd2edb173303cb513fe0c6db0cf58fec0108fb2a72d259bd30615bf36fcff0b9ed81cdbf2e02627181fadbaf57329cf667eccca8f6690efca5a318ced2feea'
