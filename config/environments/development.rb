27aaccd3e311216f5c73abd21f2959f3f9de6c45
Omkar <hiomkar@gmail.com>
2012-10-31 01:03:58 -0400

    fix for devise => hostname in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,6 @@ Helpr::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'livehelpr.herokuapp.com' }
 
 end
