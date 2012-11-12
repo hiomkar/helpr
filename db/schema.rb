ba89aef6bdb3b4efc54031429bb536e08c215eec
Omkar <hiomkar@gmail.com>
2012-11-12 14:43:14 -0500

    integration

--- db/schema.rb
+++ db/schema.rb
@@ -88,22 +88,10 @@ ActiveRecord::Schema.define(:version => 20121108034318) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "phone"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
-
-  add_index "customers", ["email"], :name => "index_customers_on_email", :unique => true
-  add_index "customers", ["reset_password_token"], :name => "index_customers_on_reset_password_token", :unique => true
 
   create_table "messages", :force => true do |t|
     t.integer  "chat_id"
