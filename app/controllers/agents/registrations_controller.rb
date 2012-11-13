7ab4b8d0bf3d98c0318d1e3f7b085345119ad7c6
Mike Ferraco <mferraco@andrew.cmu.edu>
2012-11-13 16:06:00 -0500

    can sign up agents

--- app/controllers/agents/registrations_controller.rb
+++ app/controllers/agents/registrations_controller.rb
@@ -8,10 +8,34 @@ class Agents::RegistrationsController < Devise::RegistrationsController
     @business = @cur_admin.business
     super
   end
+  
+  # POST /resource
+  def create
+    build_resource
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        # removed this becuase we don't need to sign in the agent upon creation
+        #sign_in(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+  
+  def after_sign_up_path_for(resource)
+    if (resource.is_a?(Agent))
+      new_agent_registration_path
+    else
+       super
+    end
+  end
 
 end
-
-# Need to
-#--get association between businesses and agents working
-#--don't sign in after creating agent and leave admin signed in
-#--redirect to create agent page to create another agent
