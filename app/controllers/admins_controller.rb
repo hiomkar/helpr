e3ad700e71b6d2b51e0d1b46299a2cbcbc226bfd
Mike Ferraco <mferraco@andrew.cmu.edu>
2012-11-15 09:56:22 -0500

    adding links to buttons on admin ui page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -15,6 +15,7 @@ class AdminsController < ApplicationController
   # GET /admins/1.json
   def show
     @admin = Admin.find(params[:id])
+    @business = @admin.business
 
     respond_to do |format|
       format.html # show.html.erb
