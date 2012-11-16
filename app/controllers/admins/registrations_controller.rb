e6d3888f5fa07a9e50682fb8bbdef70424ef0f2c
Mike Ferraco <mferraco@andrew.cmu.edu>
2012-11-15 21:46:44 -0500

    admin ui page almost done

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -1,5 +1,13 @@
 class Admins::RegistrationsController < Devise::RegistrationsController
    
+  def update
+    # converts the time into a saveable format
+    params[:admin][:business_attributes].parse_time_select! :start_hour 
+    params[:admin][:business_attributes].parse_time_select! :end_hour
+    
+    super
+  end
+   
   def create
     # converts the time into a saveable format
     params[:admin][:business_attributes].parse_time_select! :start_hour 
