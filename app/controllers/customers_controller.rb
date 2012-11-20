1e916d62923f6329d2b02044c5aa1736c76564d4
Mike Ferraco <mferraco@andrew.cmu.edu>
2012-11-20 15:23:43 -0500

    final branch commit

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,6 @@
 require "router/agent_router"
 
 class CustomersController < ApplicationController
-
   def new
     @customer = Customer.new
     business_param = params[:business]
@@ -15,48 +14,58 @@ class CustomersController < ApplicationController
       redirect_to "/"
     end
   end
-  
 
-  def create
+  def index
     #create chat object here
-    @customer = Customer.new(params[:customer])
+    #@customer = Customer.new(params[:customer])
 
     input_customer_email = params[:customer][:email]
     existing_customer = Customer.find_by_email(input_customer_email)
 
     if input_customer_email != nil && existing_customer != nil
-      @customer = existing_customer
+    @customer = existing_customer
     else
-      # create customer here
-      #@customer = Customer.new(params[:chat][:customer])
-      #@customer.save
-      @customer = nil
+    # create customer here
+    #@customer = Customer.new(params[:chat][:customer])
+    #@customer.save
+    #@customer = nil
+
+      @customer = Customer.new
+      @customer.first_name = params[:customer][:first_name]
+      @customer.last_name = params[:customer][:first_name]
+      @customer.email = params[:customer][:email]
+      @customer.phone = params[:customer][:phone]
+
     end
 
+    @customer.save
+
+    @chat = Chat.new
+    @chat.business_id = params[:customer][:chat][:business_id]
+    @chat.customer_id = @customer.id
+    @chat.save
     #@chat.customer = @customer
 
     @chat.channel = "message_channel_" + @chat.id.to_s
     @user = ChatUser.user(session, @customer.first_name)
-    @messages = Message.for_chat(@chat.id).order_by_time
+    @messages = Message.all(:conditions => ["chat_id = ?", @chat.id.to_s])
 
     #@chat.save
 
     #business_url = params[:business_url]
     #@business = Business.find_by_biz_url(business_url)
     #@chat.business = @business
-     @customer.save
-      #params[:chat][:customer] = nil
-         
-
-    agent_id = AgentRouter.select_agent(@chat.business)
+    @customer.save
+    #params[:chat][:customer] = nil
+    @business = @chat.business
+    agent_id = AgentRouter.select_agent(@business)
 
     #notify agents of incoming chat
     channel_name = "cc-new-chat-channel-"+agent_id.to_s
     event_name = "customer-call-event"
     Pusher[channel_name].trigger(event_name, {:chat_id => @chat.id.to_s, :customer_name => @chat.customer.first_name, :customer_id => @chat.customer.id})
-  
-    render 'customers/index'
-    
+
+
   end
 
 end
